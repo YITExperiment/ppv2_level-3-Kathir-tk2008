f2e4233de6d8620573cc334643c2df02c9ea385f
Kathir Theivakumar <134518060+Kathir-tk2008@users.noreply.github.com>
2023-06-05 19:51:14 +0530

    Add files via upload

---
+++ robort_builder(Activity page).py
@@ -0,0 +1,68 @@
+import turtle as t
+def rectangle(horizontal,vertical,color):
+    t.pendown()
+    t.pensize(1)
+    t.color(color)
+    t.begin_fill()
+    for counter in range(1,3):
+        t.forward(horizontal)
+        t.right(90)
+        t.forward(vertical)
+        t.right(90)
+    t.end_fill()
+    t.penup()
+    
+t.penup()
+t.speed('normal')
+t.bgcolor('seashell')
+    
+#feet
+t.goto(-100, -150)
+rectangle(50,20,'yellow')
+t.goto(-30,-150)
+rectangle(50,20,'yellow')
+    
+#legs
+t.goto(-25, -50)
+rectangle(15,100,'hot pink')
+t.goto(-55,-50)
+rectangle(-15,100,'hot pink')
+
+#body
+t.goto(-90,100)
+rectangle(100,150,'lawn green')
+
+#arms
+t.goto(-150, 70)
+rectangle(60,15,'hot pink')
+t.goto(-150,110)
+rectangle(15,40,'yellow')
+
+#hands
+t.goto(10, 70)
+rectangle(60,15,'hot pink')
+t.goto(55,110)
+rectangle(15,40,'yellow')
+
+#neck
+t.goto(-50,120)
+rectangle(15,20,'thistle')
+
+#head
+t.goto(-85,170)
+rectangle(80,50,'light blue')
+
+#eyes
+t.goto(-63,160)
+rectangle(40,15,'black')
+t.goto(-60,160)
+rectangle(8,8,'white')
+t.goto(-35,160)
+rectangle(8,8,'white')
+
+#mouth
+t.goto(-65,135)
+t.right(0)
+rectangle(40,10,'red')
+
+t.hideturtle()
